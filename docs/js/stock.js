e30972a5bd9c572f500b3efbd593ca0d16b30d6f
qintalhq <itsmearhamkhan2012@gmail.com>
2026-02-11 14:58:06 +0530

    Update stock.js

--- docs/js/stock.js
+++ docs/js/stock.js
@@ -1,17 +1,40 @@
 import { supabase } from "./supabase.js";
 
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const ticker = params.get("ticker");
 
-async function loadStock() {
+async function loadResearch() {
   const { data } = await supabase
     .from("research")
     .select("*")
-    .eq("id", id)
+    .eq("ticker", ticker)
     .single();
 
-  document.querySelector("h1").textContent = data.company;
-  document.querySelector(".ticker").textContent = data.ticker;
+  document.getElementById("companyName").innerText =
+    `${data.company} (${data.ticker})`;
+
+  document.getElementById("overview").innerText = data.overview;
+  document.getElementById("summary").innerText = data.summary;
+  document.getElementById("country").innerText = data.country;
+  document.getElementById("updated").innerText =
+    "Last updated: " + new Date(data.created_at).toDateString();
+
+  data.positives.forEach(p => {
+    document.getElementById("positives").innerHTML += `<li>${p}</li>`;
+  });
+
+  data.risks.forEach(r => {
+    document.getElementById("risks").innerHTML += `<li>${r}</li>`;
+  });
 }
 
-loadStock();
+async function loadPrice() {
+  const res = await fetch(
+    `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=d664mv9r01qssgecgdogd664mv9r01qssgecgdp0`
+  );
+  const data = await res.json();
+  document.getElementById("price").innerText = data.c;
+}
+
+loadResearch();
+loadPrice();
