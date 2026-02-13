d61f3484fd6c69a6e408a2f0a7ef5824ade97413
qintalhq <itsmearhamkhan2012@gmail.com>
2026-02-13 19:10:00 +0530

    Update auth.js

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -1,29 +1,16 @@
 import { supabase } from "./supabase.js";
 
-// LOGIN
-export async function login(email, password) {
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
+export function requireAuth(callback) {
+  supabase.auth.onAuthStateChange((_e, session) => {
+    if (!session) {
+      location.href = "login.html";
+    } else {
+      callback(session.user);
+    }
   });
-
-  if (error) {
-    alert(error.message);
-    return false;
-  }
-
-  window.location.reload();
-  return true;
 }
 
-// LOGOUT
 export async function logout() {
   await supabase.auth.signOut();
-  window.location.reload();
-}
-
-// CHECK SESSION
-export async function getUser() {
-  const { data } = await supabase.auth.getSession();
-  return data.session?.user || null;
+  location.href = "login.html";
 }
